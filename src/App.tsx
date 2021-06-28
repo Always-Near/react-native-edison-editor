90ecb1e45ad581b4b9438beaae310bfe9f1fd2b7
yangning <ning@edison.tech>
2021-06-28 10:36:40 +0800

    Revert "ON-1379: onCompositionend has bug in draftjs"
    This reverts commit 8b3d19209cf59bed6ea002edfd85f87e5319f76f.

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,6 @@ const EventName = {
   OnPastedFiles: "onPastedFiles",
   OnPastedLocalFiles: "onPastedLocalFiles",
   OnDroppedFiles: "onDroppedFiles",
-  Debugger: "debugger",
 } as const;
 
 type State = {
@@ -93,15 +92,6 @@ class App extends React.Component<any, State> {
         this.focusTextEditor();
       }
     });
-
-    // onCompositionend has bug in draftjs, so remove it in draftjs
-    const editorContent = document.querySelector(".public-DraftEditor-content");
-    if (editorContent) {
-      editorContent.addEventListener("compositionend", (e) => {
-        e.stopPropagation();
-        e.preventDefault();
-      });
-    }
   }
 
   private postMessage = (type: string, data: any) => {
